9970b0c7bed0962024e842b3f92b9e47b47ce90a
Lim-dl <daniel.lemeshkov@gmail.com>
2022-12-15 15:35:47 +0400

    Preview

---
+++ script.js
@@ -0,0 +1,8 @@
+let fog1 = document.querySelector('.mouse-parallax-fog-1');
+let fog2 = document.querySelector('.mouse-parallax-fog-2');
+window.addEventListener('mousemove', function(e) {
+    let x = e.clientX / window.innerWidth;
+    let y = e.clientY / window.innerHeight;  
+    fog1.style.transform = 'translate(+' + x * 100 + 'px, +' + y * 100 + 'px)';
+    fog2.style.transform = 'translate(+' + x * 50 + 'px, +' + y * 50 + 'px)'; 
+});
