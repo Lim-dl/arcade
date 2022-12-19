0e14856f3381aec8a61f24e9c61826212fcd41a3
Limbo-dl <daniel.lemeshkov@gmail.com>
2022-12-19 23:20:12 +0400

    Test script

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ let fog2 = document.querySelector('.mouse-parallax-fog-2');
 window.addEventListener('mousemove', function(e) {
     let x = e.clientX / window.innerWidth;
     let y = e.clientY / window.innerHeight;  
-    fog1.style.transform = 'translate(+' + x * 100 + 'px, +' + y * 100 + 'px)';
-    fog2.style.transform = 'translate(+' + x * 50 + 'px, +' + y * 50 + 'px)'; 
+    fog1.style.transform = 'translate(-' + x * 100 + 'px, -' + y * 100 + 'px)';
+    fog2.style.transform = 'translate(-' + x * 50 + 'px, -' + y * 50 + 'px)'; 
 });
