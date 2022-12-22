5b92eb71500ebebdbc289fdf43196ff605a6b16b
Limbo-dl <daniel.lemeshkov@gmail.com>
2022-12-22 18:15:47 +0400

    add touchmvoe

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ window.addEventListener('mousemove', function(e) {
     fog2.style.transform = 'translate(-' + x * 50 + 'px, -' + y * 50 + 'px)'; 
 });
 window.addEventListener('touchmove', function(e) {
-    let x = e.clientX / window.innerWidth;
-    let y = e.clientY / window.innerHeight;  
+    let x = e.touches[0].clientX / window.innerWidth;
+    let y = e.touches[0].clientY / window.innerHeight;  
     fog1.style.transform = 'translate(-' + x * 20 + 'px, -' + y * 20 + 'px)';
     fog2.style.transform = 'translate(-' + x * 50 + 'px, -' + y * 50 + 'px)'; 
 });
