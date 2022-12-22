a33ab04dcfa8fcf0d2c52921d4f3ddef0bb6e1bf
Limbo-dl <daniel.lemeshkov@gmail.com>
2022-12-22 14:09:56 +0400

    add touchmove

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ let fog2 = document.querySelector('.mouse-parallax-fog-2');
 window.addEventListener('mousemove', function(e) {
     let x = e.clientX / window.innerWidth;
     let y = e.clientY / window.innerHeight;  
-    fog1.style.transform = 'translate(-' + x * 15 + 'px, -' + y * 15 + 'px)';
-    fog2.style.transform = 'translate(-' + x * 35 + 'px, -' + y * 35 + 'px)'; 
+    fog1.style.transform = 'translate(-' + x * 20 + 'px, -' + y * 20 + 'px)';
+    fog2.style.transform = 'translate(-' + x * 50 + 'px, -' + y * 50 + 'px)'; 
+});
+window.addEventListener('touchmove', function(e) {
+    let x = e.clientX / window.innerWidth;
+    let y = e.clientY / window.innerHeight;  
+    fog1.style.transform = 'translate(-' + x * 20 + 'px, -' + y * 20 + 'px)';
+    fog2.style.transform = 'translate(-' + x * 50 + 'px, -' + y * 50 + 'px)'; 
 });
