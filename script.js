40ca942a7f3e76f92ef3cbc732a7e4cd41d841ee
Limbo-dl <daniel.lemeshkov@gmail.com>
2023-01-12 18:33:44 +0400

    IN progress
    start page hidden

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
+/* Паралакс фон на старте */
 let fog1 = document.querySelector('.mouse-parallax-fog-1');
 let fog2 = document.querySelector('.mouse-parallax-fog-2');
-window.addEventListener('mousemove', function(e) {
+
+function moveBGstarts(e){
     let x = e.clientX / window.innerWidth;
     let y = e.clientY / window.innerHeight;  
     fog1.style.transform = 'translate(-' + x * 20 + 'px, -' + y * 20 + 'px)';
     fog2.style.transform = 'translate(-' + x * 50 + 'px, -' + y * 50 + 'px)'; 
+}
+
+window.addEventListener('mousemove', function(e) {
+    moveBGstarts(e);
 });
+
 window.addEventListener('touchmove', function(e) {
-    let x = e.touches[0].clientX / window.innerWidth;
-    let y = e.touches[0].clientY / window.innerHeight;  
-    fog1.style.transform = 'translate(-' + x * 20 + 'px, -' + y * 20 + 'px)';
-    fog2.style.transform = 'translate(-' + x * 50 + 'px, -' + y * 50 + 'px)'; 
+    moveBGstarts(e);
+});
+
+
+/* Движение объекта за курсором */
+ let spacecraft = document.querySelector('#spasecraft');
+let field = document.querySelector('.pg-game__playfield');
+field.addEventListener('mousemove', function(e) {
+    let x = e.clientX / window.innerWidth;
+    let y = e.clientY / window.innerHeight;  
+    spacecraft.style.transform = 'translate(+' + x * 1100 + 'px, +' + y * 600 + 'px)';
+    console.log(x, y);
 });
